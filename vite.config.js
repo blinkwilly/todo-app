684e58f362e733f9ee8466c06765484254c2c258
blinkwilly <wchuks58@gmail.com>
2025-10-30 19:34:08 +0100

    update: vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/todo-app/', // Repository name
+  base: './', // ✅ Correct for Netlify or any root deployment
 })
